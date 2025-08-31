c7a09eb63c01c58c85227886d5286495b788abda
gurneetBaran311 <gurneetbaran922@gmail.com>
2025-09-01 01:16:11 +0530

    Time: 1 ms (80.99%) | Memory: 59 MB (42.72%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function (s) {
+    let n = s.length - 1;
+    while (n > -1) {
+        if (Number(s[n]) % 2 == 1) {
+            return s.substring(0, n + 1);
+        }
+        --n;
+    }
+    return "";
+};
