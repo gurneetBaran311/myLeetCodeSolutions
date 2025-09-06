d5b959126efd0301cb78ba42c153e1a8ed7dd061
gurneetBaran311 <gurneetbaran922@gmail.com>
2025-09-06 22:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (77.04%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
-        for i in nums:
-            freq[i] = freq.get(i,0) + 1
-        for key, value in freq.items():
-            if value == 1:
-                return key
+        # using XOR approach as XOR cancels out duplicates. a^a = 0 and a^1=a
+        result = 0
+        for num in nums:
+            result^= num
+        return result    
 
                 
